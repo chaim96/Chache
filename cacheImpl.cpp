57194fb21e0e952b3788616919ad763b3b5609b9
chaim96 <chaim@campus.technion.ac.il>
2023-05-14 12:54:03 +0300

    1. changed stats. 2. added Counter vector to CacheTable. 3. added initial read/write methods (not finished) 4. added calc tag/set methods. 5. added search in Cache and find LRU.

--- cacheImpl.cpp
+++ cacheImpl.cpp
@@ -21,66 +21,162 @@ enum Alloc {
 };
 
 struct Stat {
-    double MissRate;
-    double accTime;
+    double numOfMiss;
     double numOfAcc;
+    double accTime;
 };
+
+
+/**********************************************
+ * CacheTable Class
+ **********************************************/
+
 
 class CacheTable{
     unsigned numEntries;
     unsigned Ways;
-    unsigned Cycles;
+    unsigned BSize;
 
     vector <unsigned> validBit;
     vector <unsigned> DirtyBit;
     vector <unsigned> Tag;
-    vector <pair<unsigned, unsigned>> BlockRange;
+    vector <unsigned> Counter;
+    //vector <pair<unsigned, unsigned>> BlockRange;
 
-    Stat stats;
+    unsigned calcSet(unsigned address);
+    unsigned calcTag(unsigned address);
+    int searchInCache(unsigned address);
+    int findLRU(unsigned address);
+
 
 public:
+    Stat stats;
     CacheTable() = default;
-    CacheTable(unsigned BSize, unsigned numEntries, unsigned Ways, unsigned Cycles): numEntries(numEntries), Ways(Ways), Cycles(Cycles){
+    CacheTable(unsigned BSize, unsigned numEntries, unsigned Ways, unsigned Cycles): numEntries(numEntries), Ways(Ways),
+                BSize(BSize){
         //initiate vectors
         for (int i = 0; i < numEntries; ++i) {
             validBit.push_back(0);
             Tag.push_back(0);
+            Counter.push_back(0);
             pair<unsigned, unsigned> p = {0, 0};
-            BlockRange.push_back(p);
+            //BlockRange.push_back(p);
         }
 
-        //todo: calc tag
-
         //initiate stats
-        stats.MissRate =0;
-        stats.accTime=0;
+        stats.numOfMiss=0;
+        stats.accTime=Cycles;
         stats.numOfAcc=0;
     }
+
+    bool read(unsigned address);
+    bool write(unsigned address);
 };
+
+
+/**********************************************
+ * CacheTable Private Methods
+ **********************************************/
+
+
+unsigned CacheTable::calcSet(unsigned address)
+{
+    int mask = (1 << (int)log2(Ways)) - 1;
+    mask = mask << (int)log2(BSize);
+    return (mask&address) >> (int)log2(BSize);
+}
+
+
+unsigned CacheTable::calcTag(unsigned address)
+{
+    return address >> ((int)log2(Ways) + (int)log2(BSize));
+}
+
+
+int CacheTable::searchInCache(unsigned address)
+{
+    int tag = calcTag(address);
+    int set = calcSet(address);
+    for (int i=0; i<Ways; i++)
+    {
+        if(Tag[set] == tag && validBit[set] == 1)
+        {
+            Counter[set]++;
+            return set;
+        }
+        set += numEntries/Ways;
+    }
+    return -1;
+}
+
+
+int CacheTable::findLRU(unsigned address)
+{
+    int set = calcSet(address);
+    int min = Counter[set];
+    int minSet = set;
+    for (int i=0; i<Ways; i++)
+    {
+        if(Counter[set] < min)
+        {
+           minSet = set;
+        }
+        set += numEntries/Ways;
+    }
+    return minSet;
+}
+
+
+/**********************************************
+ * CacheTable Public Methods
+ **********************************************/
+
+
+bool CacheTable::read(unsigned address)
+{
+    stats.numOfAcc++;
+    if (searchInCache(address) != -1)
+    {
+        return true;
+    }
+    stats.numOfMiss++;
+    return false;
+}
+
+
+bool CacheTable::write(unsigned address)
+{
+    stats.numOfAcc++;
+    int set = searchInCache(address);
+    if (set != -1)
+    {
+        DirtyBit[set] = 1;
+        return true;
+    }
+    stats.numOfMiss++;
+}
+
+
+/**********************************************
+ * Cache Class
+ **********************************************/
+
 
 class Cache {
 private:
     unsigned MemCyc;
     unsigned BSize;
     unsigned Allocate;
-
-
     CacheTable L1;
     CacheTable L2;
 
-    int searchInCache(unsigned tag);
-
 public:
     Cache() = default;
-
     void Cache_initiate(unsigned MemCyc, unsigned BSize, unsigned L1Size, unsigned L2Size, unsigned L1Cyc,unsigned L2Cyc,
                                unsigned L1Assoc, unsigned L2Assoc, unsigned WrAlloc);
-
     Cache(Cache const &) = delete; // disable copy ctor
     void operator=(Cache const &) = delete; // disable = operator
-
     ~Cache() = default;
-
     static Cache &getInstance() // make singleton
     {
         static Cache instance; // Guaranteed to be destroyed.
@@ -88,14 +184,15 @@ public:
         return instance;
     }
 
-
     bool writeToCache(unsigned address);
     bool readFromCache(unsigned address);
-
     void CacheGetStats(Stat * statsL1, Stat *statsL2);
-
 };
 
+
+/**********************************************
+ * Cache Methods
+ **********************************************/
 
 
 void Cache::Cache_initiate(unsigned MemCyc, unsigned BSize, unsigned L1Size, unsigned L2Size, unsigned L1Cyc,unsigned L2Cyc,
@@ -108,18 +205,33 @@ void Cache::Cache_initiate(unsigned MemCyc, unsigned BSize, unsigned L1Size, uns
     unsigned num_blocks1 = L1Size/BSize;
     unsigned num_blocks2 = L2Size/BSize;
 
-
-
     this->L1 = CacheTable(BSize, num_blocks1, L1Cyc, L1Assoc);
     this->L2 = CacheTable(BSize, num_blocks2, L2Cyc, L2Assoc);
-
-
-
 };
+
+
+bool Cache::readFromCache(unsigned address)
+{
+    if (!L1.read(address))
+    {
+        L2.read(address);
+    }
+}
+
+
+bool Cache::writeToCache(unsigned address)
+{
+    if (!L1.write(address))
+    {
+        L2.write(address);
+    }
+}
+
 
 /**********************************************
  * Main Functions
  **********************************************/
+
 
 int  Cache_init(unsigned MemCyc, unsigned BSize, unsigned L1Size, unsigned L2Size, unsigned L1Cyc,unsigned L2Cyc,
                 unsigned L1Assoc, unsigned L2Assoc, unsigned WrAlloc){
@@ -150,8 +262,7 @@ void Cache_GetStats(double * L1MissRate, double * L2MissRate, double * avgAccTim
 
     cache.CacheGetStats(&statsL1, &statsL2);
 
-    *L1MissRate = statsL1.MissRate;
-    *L2MissRate = statsL2.MissRate;
-    *avgAccTime = (statsL1.accTime + statsL2.accTime)/ (statsL1.numOfAcc+statsL2.numOfAcc);
-
+    *L1MissRate = statsL1.numOfMiss/statsL1.numOfAcc;
+    *L2MissRate = statsL2.numOfMiss/statsL2.numOfAcc;
+    *avgAccTime = (statsL1.accTime*statsL1.numOfAcc + statsL2.accTime*statsL2.numOfAcc) / (statsL1.numOfAcc+statsL2.numOfAcc);
 }
